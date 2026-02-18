bbba8f7eb02e91af32f2b65fd90e0c6ac7185c19
Yash Goyal <yash.goyal06@proton.me>
2026-02-18 22:57:59 +0530

    Update urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('verify-user/', views.verify_user),
-    path('register/', views.register_participant),
-    path('trigger-matching/', views.trigger_matching),
-    path('get-my-match/', views.get_my_match)
+    path('verify/', views.verify_user, name='verify_user'),
+    path('register/', views.register_participant, name='register_participant'),
+    path('register-duo/', views.register_duo, name='register_duo'), # New Path
+    path('my-match/', views.get_my_match, name='get_my_match'),
+    path('participants/', views.get_participants, name='get_participants'),
+    path('matches/', views.get_matches, name='get_matches'),
+    path('trigger-matching/', views.trigger_matching, name='trigger_matching'),
 ]
